cf4ea1a7095bc043aaf37e06cda3a6a846069d44
Ohanna Dezidério <ohannadeziderio@gmail.com>
2021-12-12 22:42:07 -0300

    [ohannadeziderio] - Wallet - Initial commit

---
+++ api/src/test/java/com/wallet/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
